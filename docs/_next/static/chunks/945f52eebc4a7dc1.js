2b9785058fdae56506cbff34d4f9ae01d8b4e498
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-15 21:50:37 +0700

    update prettier

--- docs/_next/static/chunks/945f52eebc4a7dc1.js
+++ docs/_next/static/chunks/945f52eebc4a7dc1.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/", [
-  "static/chunks/9eb635771d02c37b.js",
+  "static/chunks/8d7f519f7f7a69d8.js",
   "static/chunks/f787752f9c8f4534.js",
   "static/chunks/0a1bd100d86d94a0.js",
-  "static/chunks/turbopack-a469314403fdeca8.js"
+  "static/chunks/turbopack-3ee84735b4078ea0.js"
 ])
