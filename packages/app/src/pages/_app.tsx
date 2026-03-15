9c152865cd2a4aad56b1dc7b646463cd483f2a73
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-15 21:31:17 +0700

    update prettier

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -4,17 +4,17 @@ import Head from 'next/head';
 import { FC } from 'react';
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>Towers of Hanoi</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Towers of Hanoi</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default App;
--- packages/app/src/pages/_document.tsx
+++ packages/app/src/pages/_document.tsx
@@ -2,15 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import { FC } from 'react';
 
 const Document: FC = () => {
-  return (
-    <Html lang="en" data-theme="luxury">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en" data-theme="luxury">
+			<Head />
+			<body className="antialiased">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 };
 
 export default Document;
